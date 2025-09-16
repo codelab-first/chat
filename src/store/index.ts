2a65079cd875e88784ea3d9540e86c24256e688b
kimsunghyun <berserk.gahz@gmail.com>
2025-09-16 13:21:36 +0900

    add store

---
+++ src/store/index.ts
@@ -0,0 +1,47 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import sessionStorage from "redux-persist/es/storage/session";
+import authReducer from "./slices/auth-slice";
+import chatReducer from "./slices/chat-slice";
+
+const authConfig = { key: "auth", storage: sessionStorage };
+
+const reducers = combineReducers({
+  auth: persistReducer(authConfig, authReducer),
+  chat: chatReducer,
+});
+
+const createStore = () => {
+  const store = configureStore({
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+        },
+      }),
+  });
+
+  return store;
+};
+const store = createStore();
+const persistor = persistStore(store, null, () => {
+  console.log("Persisted state loaded");
+});
+// getUser();
+
+export default {store,persistor};
+export type RootState = ReturnType<typeof store.getState>;
+
+// const getUser = () => {
+//   try {
+//     const user = localStorage.getItem("user");
+//     if (!user) return;
+//     store.dispatch(userActions.check());
+//     store.dispatch(chatActions.getChats());
+//   } catch (e) {
+//     console.log("local storage is not working");
+//   }
+// };
+
+// import localStorage from "redux-persist/es/storage";
