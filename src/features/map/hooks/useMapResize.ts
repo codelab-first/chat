9e15e778f4cabcffd8b528632ce02eadc8d04318
LilyMorpho <ikaros9903@daum.net>
2025-09-19 12:01:01 +0900

    지도의 center를 초기값이 아닌 현재 위치를 중심으로 고정되게 변경

--- src/features/map/hooks/useMapResize.ts
+++ src/features/map/hooks/useMapResize.ts
@@ -1,21 +1,28 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export default function useMapResize() {
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
+  const centerRef = useRef<kakao.maps.LatLng | null>(null);
 
   useEffect(() => {
     if (!map) return;
 
-    const center = map.getCenter();
+    centerRef.current = map.getCenter();
+
+    const updateCenter = () => {
+      centerRef.current = map.getCenter();
+    };
 
     const handleResize = () => {
+      if (!map ||!centerRef.current) return;
       map.relayout();
-      map.setCenter(center);
+      map.setCenter(centerRef.current);
     }; 
-
+    kakao.maps.event.addListener(map, "center_changed", updateCenter);
     window.addEventListener("resize", handleResize);
 
     return () => {
+      kakao.maps.event.removeListener(map, "center_changed", updateCenter);
       window.removeEventListener("resize", handleResize);
     };
   }, [map]);
