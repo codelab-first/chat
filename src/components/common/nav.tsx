4b1457170aa3b485ab72610bbb37a6e8b26aeacd
kimsunghyun <berserk.gahz@gmail.com>
2025-09-22 13:13:24 +0900

    채팅창 토글가능

--- src/components/common/nav.tsx
+++ src/components/common/nav.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled'
-
+import { formSelector, formActions } from '../../store/slices/form-slice';
+import { useSelector, useDispatch } from "react-redux";
 const WrapsNav = styled.ul`
 margin-left:auto;
 width:30%;
@@ -13,12 +14,18 @@ padding:1em 1.5em;
 
 `
 const Nav = () => {
+
+  const dispatch = useDispatch()
+  const { chatting } = useSelector(formSelector)
+
+  const onClick = () => { dispatch(formActions.toggle_form({ form: 'chatting', value: !chatting.visible })) }
   return (
     <div>
       <WrapsNav>
         {/* <li><a href="/weather">날씨정보</a></li> */}
         <li><a href="/home">대기정보</a></li>
         <li><a href="/weather">날씨정보</a></li>
+        <button onClick={onClick}>채팅창</button>
       </WrapsNav>
     </div>
   );
