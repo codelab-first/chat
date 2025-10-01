74c20e79710ae330d3bbe3049b73d60a98cc2d7b
Lilymorpho <ikaros9903@daum.net>
2025-10-01 10:43:09 +0900

    시도별 데이터 화면에 띄우기 & 현 위치 정보와 버튼을 누르면 토글되게

--- src/app/air/AirLocal.tsx
+++ src/app/air/AirLocal.tsx
@@ -3,25 +3,7 @@ import useCurrentLocation from "../../hooks/useCurrentLocation"
 import axios from "axios"
 import { getGradeText, getGradeColor } from "../../utils/getGrade"
 
-interface AirData {
-  stationName: string
-  pm10Grade: number | null
-  pm25Grade: number | null
-  khaiGrade: number | null
-  so2Grade: number | null
-  o3Grade: number | null
-  no2Grade: number | null
-  coGrade: number | null
-  pm10Value: number | null
-  pm25Value: number | null
-  khaiValue: number | null
-  so2Value: number | null
-  o3Value: number | null
-  no2Value: number | null
-  coValue: number | null
-  dataTime: string
-  sidoName: string
-}
+import { AirData } from "../../types"
 
 type Props = {
   onShowApp?: () => void
