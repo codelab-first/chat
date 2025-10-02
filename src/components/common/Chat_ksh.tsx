74ddd04fe1543830a9be112e7045370399610302
0xotashi <soheek178@gmail.com>
2025-10-02 14:32:42 +0900

    채팅창 ui 수정

--- src/components/common/Chat_ksh.tsx
+++ src/components/common/Chat_ksh.tsx
@@ -5,19 +5,18 @@ import Button from './Button'
 import { useSelector, useDispatch } from "react-redux";
 import { formSelector, formActions } from '../../store/slices/form-slice';
 import { useDrag } from '@use-gesture/react';
-import { chatData, chatActions } from '../../store/slices/chat-slice'
+import { chatData } from '../../store/slices/chat-slice'
 import { tokenData } from '../../store/slices/token-slice'
 import axios from 'axios';
+import { FaCalendarAlt, FaPaperPlane, FaCamera } from "react-icons/fa";
 import { imageInsert } from '../../modules/createFormData'
 import { io } from "socket.io-client";
 import './chat.scss'
 
-import { apiPost } from '../../modules/api';
 import { authData } from '../../store/slices/auth-slice';
 
 
 const Wraps = styled.div`
-// border:1px solid black;
 width:400px;
 padding:1em;
 position:relative;
@@ -27,27 +26,21 @@ margin:0 auto;
 display:flex;
 justify-content:space-between;
 align-items:center;
-
 `
 const WrapChat = styled.div`
-border:1px solid black;
-
 margin:1em auto;
 width:90%;
 height:500px;
 background:white;
-box-shadow:0px 0px 8px 4px  rgba(.3,.3,.3,.3);
+box-shadow: 0px 4px 12px rgba(0,0,0,0.15); 
 overflow-y:scroll;
-
-
-
 `
 const WrapControl = styled.div`
 margin:0 auto;
 display:flex;
 justify-content:flex-end;
 align-items:center;
-// width:70%;
+width:90%;
 `
 const CircleBtn = styled.div`
 min-width:30px;
@@ -66,22 +59,11 @@ user-select:none;
 &:hover{
   color:white;
   background:black;
-  // transform:rotate(360deg)
-  }
-  // &:active{
-  //   transform:rotate(360deg)
-  //   }
-    `
+}
+`
 
-// const MessageDiv = styled.div<{ wrap: string, height: number }>`
-//     ${props => props.wrap && css`text-wrap:${props.wrap}`};
-//     // ${props => props.height && css`height:${((props.height - 1) * 2)}`};
-
-//   // textWrap: 'wrap', width: "150px", height: "10px", background: 'yellow', position: 'absolute'
-//   `
 const Chat = () => {
   const scrollRef = useRef<HTMLDivElement>(null)
-  // const { userData } = useSelector(authData)
   const { chatting } = useSelector(formSelector)
   const { token, user } = useSelector(tokenData)
   const [rise, setRise] = useState(false)
@@ -91,11 +73,10 @@ const Chat = () => {
   }
   const chattingPos = useDrag(params => { changePosition('chatting', { x: params.offset[0] + 600, y: params.offset[1] + 100 }) })
 
-  const { imageList, status, messages } = useSelector(chatData)
+  const { imageList } = useSelector(chatData)
   const [message, setMessage] = useState('')
   const [chats, setChats] = useState<{ chat: string, name: string, image: string[] }[]>([])
   const [day, setDay] = useState<{ [key: string]: string }>({});
-
 
   const daySelect = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -106,20 +87,18 @@ const Chat = () => {
     const { value } = e.target;
     setMessage(value)
   }
+
   const onInsertImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    // imageList가 비어있으면 비어 있는대로 => 생성
-    // imageList에 데이터가 있으면 있는대로 => 수정
-    // 결과적으로 imageList와 조합해서 새로운 formData를 만들어 주는 함수인 것
     const formData = imageInsert(e, imageList)
-    // console.log(formData)
-    // dispatch(chatActions.addImage(await formData))
     sendImage(await formData)
   }
+
   const onSubmit = async (e: any) => {
     e.preventDefault();
     send();
     setMessage('')
   }
+
   const sendImage = async (formData: FormData) => {
     formData.append('user', JSON.stringify(user))
     return await axios.post('http://localhost:3000/chat/images', formData, {
@@ -127,14 +106,13 @@ const Chat = () => {
         "Content-Type": "multipart/form-data",
       }
     })
-
   }
-  const send = async () => {
 
-    // console.log('message', message)
+  const send = async () => {
     if (!message) return;
     return await axios.post('http://localhost:3000/chat/chat', { message, user })
   }
+
   const chatInit = async () => {
     const result = await axios.get('http://localhost:3000/chat/init')
     setChats([])
@@ -145,42 +123,37 @@ const Chat = () => {
     })
     setChats(result.data)
   }
+
   const scrollToBottom = () => {
     if (scrollRef.current) {
       scrollRef.current.scrollTop = scrollRef.current?.scrollHeight;
     }
   }
 
-
   useEffect(() => {
     const socket = io('http://localhost:3000', {
       auth: {
-        token: token.accessToken // auth 속성을 통해 토큰을 전달
+        token: token.accessToken
       }
     });
     socket.on('connect', () => {
       console.log('서버에 연결되었습니다.');
     });
-
     socket.on('disconnect', () => {
       console.log('서버와 연결이 끊어졌습니다.');
     });
-
     socket.on('message', (data: { chat: string, name: string, image: string[] }) => {
-      console.log('data', data)
       setChats(prev => [...prev, data])
     })
-
     socket.on('connect_error', (error) => {
       console.error('연결 오류:', error.message);
     });
   }, [])
 
-
-
   const riseUp = () => {
     setRise(!rise)
   }
+
   useEffect(() => {
     setTimeout(scrollToBottom, 100)
   }, [chats])
@@ -189,19 +162,14 @@ const Chat = () => {
     const result = await axios(`http://127.0.0.1:3000/chat/searchByDay?startDay=${day.startDay}&endDay=${day.endDay}`)
     setChats([])
     setChats(result.data)
-
   }
+
   useEffect(() => {
     if (chatting.visible) {
-
-
       chatInit()
-
-
-
-      // alert('채팅창 열림')
     }
   }, [chatting.visible])
+
   return (
     <div>
       {chatting.visible && <Wraps>
@@ -213,74 +181,191 @@ const Chat = () => {
           zIndex: 2,
           textAlign: 'center',
           boxSizing: 'border-box',
-
         }}>
           <div style={{ width: '480px', padding: '2rem 0', userSelect: 'none' }}></div>
         </div>
-        <div style={{ position: 'fixed', top: chatting.position.y, left: chatting.position.x, zIndex: 1, }}>
 
-          <div style={{ maxWidth: "100%", background: "lightyellow", border: "1px solid black", padding: "10px", marginTop: "2em" }}>
+        <div style={{ position: 'fixed', top: chatting.position.y, left: chatting.position.x, zIndex: 1 }}>
+          <div style={{
+            maxWidth: "100%",
+            background: "rgba(255, 255, 255, 0.8)",
+            border: "3px solid #2E7D32",
+            borderRadius: "12px",
+            padding: "1.5em",
+            marginTop: "2em"
+          }}>
+            <WrapChat ref={scrollRef} onClick={() => { if (rise) riseUp() }}>
+              <div className="chats">
+                {chats?.map((message, index) => (
+                  <div key={index}>
+                    <div className='username' style={{ marginTop: "1.5em", marginLeft: ".3em", fontSize: '.8em', color: 'gray' }}>
+                      {message.name === 'system' ? '' : message.name === user?.name ? " " : message.name}
+                    </div>
 
-
-
-            <WrapChat ref={scrollRef} onClick={() => {
-              if (rise)
-                riseUp()
-            }}>
-              <div className="chats" >
-                {chats?.map((message, index) => {
-
-                  // console.log('message.name', message.name, 'auth.name:', user?.name)
-                  return (
-                    <div key={index}>
-
-                      <div className='username' style={{ marginTop: "1.5em", marginLeft: ".3em", fontSize: '.8em', color: 'gray' }}>
-                        {message.name === 'system' ? '' : message.name === user?.name ? " " : message.name}
-                      </div>
-
-                      {message.chat && <div>
-                        <div style={{ background: 'yellow', padding: '1em .4em', borderRadius: '4px', fontSize: '1em' }} className={`chat ${message.name === 'system' ? 'center' : message?.name === user?.name ? 'right' : 'left'}`}>{message.chat && message.chat}</div>
-                      </div>}
-
+                    {/* 글 메시지 */}
+                    {message.chat && (
                       <div>
-                        {(message?.image || message?.image?.length > 0) && <div style={{ borderRadius: '4px', fontSize: '1em' }} className={`chat ${message.name === 'system' ? 'center' : message?.name === user?.name ? 'right' : 'left'}`}>
-
-                          {message?.image && (Array.isArray(message?.image) ? message.image.map(img =>
-                            <img key={img} src={`http://localhost:3000${img}`} alt='img' width='100px'></img>
-                          ) : <img key={index} src={`http://localhost:3000${message?.image}`} alt='img' width='100px' />)}
-                        </div>}
+                        <div
+                          style={{
+                            background: message?.name === user?.name
+                              ? "rgba(255, 245, 157, 0.85)" // 🟡 내가 보낸 메시지
+                              : "rgba(200, 230, 201, 0.8)", // 🟢 받은 메시지
+                            padding: "1em .4em",
+                            borderRadius: "8px",
+                            fontSize: "1em",
+                            maxWidth: "60%",
+                            display: "inline-block"
+                          }}
+                          className={`chat ${message.name === 'system'
+                            ? 'center'
+                            : message?.name === user?.name
+                              ? 'right'
+                              : 'left'}`}
+                        >
+                          {message.chat}
+                        </div>
                       </div>
-                      <div>
+                    )}
 
+                    {/* 이미지 메시지 (글메세지와 동일한 말풍선 양식 적용) */}
+                    {(message?.image || message?.image?.length > 0) && (
+                      <div
+                        style={{
+                          background: message?.name === user?.name
+                            ? "rgba(255, 245, 157, 0.85)" // 🟡 내가 보낸 이미지
+                            : "rgba(200, 230, 201, 0.8)", // 🟢 받은 이미지
+                          padding: "1em .4em",
+                          borderRadius: "8px",
+                          fontSize: "1em",
+                          maxWidth: "60%",
+                          display: "inline-block",
+                          marginTop: "0.5em"
+                        }}
+                        className={`chat ${message.name === 'system'
+                          ? 'center'
+                          : message?.name === user?.name
+                            ? 'right'
+                            : 'left'}`}
+                      >
+                        {message?.image && (Array.isArray(message?.image)
+                          ? message.image.map(img => (
+                            <img
+                              key={img}
+                              src={`http://localhost:3000${img}`}
+                              alt="img"
+                              style={{
+                                borderRadius: "6px",
+                                display: "block",
+                                margin: "4px auto",
+                                maxWidth: "100%"
+                              }}
+                            />
+                          ))
+                          : <img
+                              key={index}
+                              src={`http://localhost:3000${message?.image}`}
+                              alt="img"
+                              style={{
+                                borderRadius: "6px",
+                                display: "block",
+                                margin: "4px auto",
+                                maxWidth: "100%"
+                              }}
+                            />
+                        )}
                       </div>
-                    </div>)
-                })}
+                    )}
+                  </div>
+                ))}
               </div>
             </WrapChat>
+
             <div className={`menu ${rise ? "up" : ''}`}>
               <WrapSearch className={`search ${rise ? '' : 'down'}`}>
-                <div style={{ marginTop: '5em' }}>
-                </div>
+                <div style={{ marginTop: '5em' }}></div>
                 <input type="date" name="startDay" id="date" value={day.startDay} onChange={daySelect} />
                 <input type="date" name="endDay" id="date" value={day.endDay} onChange={daySelect} />
                 <Button color={"white"} width={'100px'} bgcolor="darkcyan" onClick={onSearch}>검색</Button>
               </WrapSearch>
             </div>
-            <WrapControl>
-              <CircleBtn onClick={riseUp} className={rise ? 'rise' : ''}>+</CircleBtn>
 
-              <form className="control" onSubmit={onSubmit}>
-                <input type="text" onChange={onChange} value={message} />
-                <Button color={"white"} width={'100px'} bgcolor="darkcyan" marginLeft=".5em">전송</Button>
-                <label htmlFor="photo" className='btn'>사진</label>
-                <input type="file" name="images" id="photo" onChange={onInsertImage} multiple accept='image/*' />
-              </form>
-            </WrapControl>
+<WrapControl>
+  {/* 달력 버튼 (회색 원) */}
+  <div
+    style={{
+      minWidth: "40px",
+      height: "40px",
+      borderRadius: "50%",
+      background: "#ccc",       // 입력창 보더와 같은 회색
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      color: "white",
+      fontSize: "1rem",
+      marginRight: "0.5em",
+      cursor: "pointer"
+    }}
+  >
+    <FaCalendarAlt />
+  </div>
+
+  <form className="control" onSubmit={onSubmit} style={{ display: "flex", alignItems: "center" }}>
+    <input type="text" onChange={onChange} value={message} />
+
+    {/* 전송 버튼 (라운드 네모, 텍스트+아이콘) */}
+    <button
+      type="submit"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "4px",
+        padding: "0.55em 0.8em",
+        borderRadius: "8px",
+        background: "#388E3C",
+        color: "white",
+        border: "none",
+        marginLeft: "0.5em",
+        cursor: "pointer"
+      }}
+    >
+    <FaPaperPlane /> 전송
+    </button>
+
+    {/* 사진 버튼 (연두색 원) */}
+    {/* 사진 버튼 (연두색 동그라미, 네모 제거) */}
+<label
+  htmlFor="photo"
+  style={{
+    display: "inline-flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "40px",
+    height: "40px",
+    borderRadius: "50%",       // 동그라미
+    background: "#81C784",     // 초록색
+    color: "white",
+    cursor: "pointer",
+    marginLeft: "0.5em",
+    border: "none"             // 네모 테두리 제거
+  }}
+>
+  <FaCamera />
+</label>
+<input
+  type="file"
+  id="photo"
+  name="images"
+  multiple
+  accept="image/*"
+  style={{ display: "none" }}
+  onChange={onInsertImage}
+/>
+  </form>
+</WrapControl>
           </div>
         </div>
-
-      </Wraps >}
-    </div >
+      </Wraps>}
+    </div>
   );
 };
 
